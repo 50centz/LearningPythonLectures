1d81b8c3971cc5a69c233a7be2c8dbdada60352b
Федоров Евгений Дом ПК <zen-g@mail.ru>
2022-12-10 18:15:49 +0300

    Git init

---
+++ Lecture1/lec.py
@@ -0,0 +1,18 @@
+print('Hello world')
+
+
+# Типы данных справедливы
+# int, float, boolean, str, list, None
+
+# value = None
+# print(type(value))
+# a = 123
+# b = 1.23
+
+# print(type(a))
+# print(type(b))
+# value = 123456
+# print(type(value))
+
+s = 'hello world'
+print(type(s)) # Вывод строки
